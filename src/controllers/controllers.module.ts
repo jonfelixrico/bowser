022c10df51ed71bf2ea022b9d8db9fbeb9e2b808
Felix <jonfelixrico@gmail.com>
2021-07-29 22:32:48 +0800

    Import the CqrsModule in ControllersModule

--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { CqrsModule } from '@nestjs/cqrs'
 import { CommandsController } from './commands/commands.controller'
 
 @Module({
+  imports: [CqrsModule],
   controllers: [CommandsController],
 })
 export class ControllersModule {}
