28419e94744f024af1cfaad6699e5cde5296214a
Felix <jonfelixrico@gmail.com>
2021-07-29 22:46:42 +0800

    Generate module for command handlers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { TurtleWsModule } from './turtle-ws/turtle-ws.module'
 import { ControllersModule } from './controllers/controllers.module'
+import { CommandHandlersModule } from './command-handlers/command-handlers.module'
 
 @Module({
-  imports: [TurtleWsModule, ControllersModule],
+  imports: [TurtleWsModule, ControllersModule, CommandHandlersModule],
 })
 export class AppModule {}
