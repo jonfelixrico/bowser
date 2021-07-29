42b08f799ed169b05ac0405bf26bda3bbe19dd14
Felix <jonfelixrico@gmail.com>
2021-07-29 23:53:49 +0800

    Integrate nest-winston into the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
 import { TurtleWsModule } from './turtle-ws/turtle-ws.module'
 import { ControllersModule } from './controllers/controllers.module'
+import { LoggerModule } from './logger/logger.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [TurtleWsModule, ControllersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.dev.env'],
+      isGlobal: true,
+    }),
+    LoggerModule,
+    TurtleWsModule,
+    ControllersModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { WsAdapter } from '@nestjs/platform-ws'
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useWebSocketAdapter(new WsAdapter(app))
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
   await app.listen(3000)
 }
 bootstrap()
