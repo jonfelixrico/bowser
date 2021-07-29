3bf953f9be19d870716abdb61e6dd4af394d759b
Felix <jonfelixrico@gmail.com>
2021-07-29 22:28:38 +0800

    Create module for controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TurtleWsModule } from './turtle-ws/turtle-ws.module'
+import { ControllersModule } from './controllers/controllers.module'
 
 @Module({
-  imports: [TurtleWsModule],
+  imports: [TurtleWsModule, ControllersModule],
   controllers: [AppController],
   providers: [AppService],
 })
