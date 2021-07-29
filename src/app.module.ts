82cfd7bf433e65612419e3e4196f7321e1b5ca6a
Felix <jonfelixrico@gmail.com>
2021-07-29 22:29:21 +0800

    Remove placeholder code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { TurtleWsModule } from './turtle-ws/turtle-ws.module'
 import { ControllersModule } from './controllers/controllers.module'
 
 @Module({
   imports: [TurtleWsModule, ControllersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
