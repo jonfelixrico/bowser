b5334dc31cafb4fc3c1a44db9663a07858cf7265
Felix <jonfelixrico@gmail.com>
2021-07-29 22:46:42 +0800

    Revert "Generate module for command handlers"
    This reverts commit 28419e94744f024af1cfaad6699e5cde5296214a.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common'
 import { TurtleWsModule } from './turtle-ws/turtle-ws.module'
 import { ControllersModule } from './controllers/controllers.module'
-import { CommandHandlersModule } from './command-handlers/command-handlers.module'
 
 @Module({
-  imports: [TurtleWsModule, ControllersModule, CommandHandlersModule],
+  imports: [TurtleWsModule, ControllersModule],
 })
 export class AppModule {}
