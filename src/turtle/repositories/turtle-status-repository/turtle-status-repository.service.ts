48db7e102c4d907cc0073310beb48e5f5cc71569
Felix <jonfelixrico@gmail.com>
2021-07-31 18:02:15 +0800

    Modify function signature, add frozen getter

--- src/turtle/repositories/turtle-status-repository/turtle-status-repository.service.ts
+++ src/turtle/repositories/turtle-status-repository/turtle-status-repository.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common'
 
+interface ITurtleStatus {
+  x: number
+  y: number
+  z: number
+  bearing: number
+  fuelLevel: number
+  fuelLimit: number
+  label: string
+}
+
+interface IStoredTurtleStatus extends ITurtleStatus {
+  id: string
+}
+
 @Injectable()
-export class TurtleStatusRepositoryService {}
+export class TurtleStatusRepositoryService {
+  private store: Record<string, Readonly<IStoredTurtleStatus>> = {}
+
+  updateStatus(id: string, status: ITurtleStatus) {
+    this.store[id] = Object.freeze({
+      ...status,
+      id,
+    })
+  }
+
+  get statusMap() {
+    return Object.freeze(this.store)
+  }
+}
