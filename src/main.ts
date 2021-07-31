33e6525e9b07946b7834382abd47f91b6930d1de
Felix <jonfelixrico@gmail.com>
2021-07-31 23:24:53 +0800

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useWebSocketAdapter(new WsAdapter(app))
+  // TODO remove during prod, maybe?
+  app.enableCors()
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
   await app.listen(3000)
 }
