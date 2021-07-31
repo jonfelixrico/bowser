d9ae8199d16c347325c600daa87deddd46c47311
Felix <jonfelixrico@gmail.com>
2021-07-31 18:06:14 +0800

    Remove the turtle module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
-import { TurtleModule } from './turtle/turtle.module'
 import { ControllersModule } from './controllers/controllers.module'
 import { LoggerModule } from './logger/logger.module'
 import { ConfigModule } from '@nestjs/config'
+import { TurtleClientPoolService } from './gateways/turtle-client-pool/turtle-client-pool.service'
+import { TurtleWsGateway } from './gateways/turtle-ws-gateway/turtle-ws.gateway'
+import { SendMessageToTurtleCommandHandlerService } from './command-handlers/send-message-to-turtle-command-handler/send-message-to-turtle-command-handler.service'
+import { TurtleStatusRepositoryService } from './repositories/turtle-status-repository/turtle-status-repository.service'
 
 @Module({
   imports: [
@@ -11,8 +14,14 @@ import { ConfigModule } from '@nestjs/config'
       isGlobal: true,
     }),
     LoggerModule,
-    TurtleModule,
     ControllersModule,
+  ],
+
+  providers: [
+    TurtleClientPoolService,
+    TurtleWsGateway,
+    SendMessageToTurtleCommandHandlerService,
+    TurtleStatusRepositoryService,
   ],
 })
 export class AppModule {}
