c54a687ca897a6c31a75e47aca695133cc851d2e
Felix <jonfelixrico@gmail.com>
2021-07-31 17:49:55 +0800

    Relocate SendMessageToTurtleCommandHandler to `turtle/command-handlers`

--- src/turtle/turtle.module.ts
+++ src/turtle/turtle.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { TurtleClientPoolService } from './gateways/turtle-client-pool/turtle-client-pool.service'
 import { CqrsModule } from '@nestjs/cqrs'
-import { SendMessageToTurtleCommandHandlerService } from './send-message-to-turtle-command-handler/send-message-to-turtle-command-handler.service'
+import { SendMessageToTurtleCommandHandlerService } from './command-handlers/send-message-to-turtle-command-handler/send-message-to-turtle-command-handler.service'
 import { TurtleWsGateway } from './gateways/turtle-ws-gateway/turtle-ws.gateway'
 
 @Module({
