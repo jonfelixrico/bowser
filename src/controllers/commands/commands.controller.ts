4dd139d99a134f31b331097b7048f8d96b9722b1
Felix <jonfelixrico@gmail.com>
2021-08-01 19:05:00 +0800

    Change the structure passed on to SendMessageToTurtleCommand

--- src/controllers/commands/commands.controller.ts
+++ src/controllers/commands/commands.controller.ts
@@ -2,7 +2,12 @@ import { Body, Post } from '@nestjs/common'
 import { Controller } from '@nestjs/common'
 import { CommandBus } from '@nestjs/cqrs'
 import { SendMessageToTurtleCommand } from 'src/commands/send-message-to-turtle.command'
-import { SendCommandDto } from '../dtos/send-command.dto'
+import { IAction, SendCommandDto } from '../dtos/send-command.dto'
+
+interface ICommandMessage {
+  type: 'COMMAND'
+  payload: IAction[]
+}
 
 @Controller('commands')
 export class CommandsController {
@@ -12,11 +17,16 @@ export class CommandsController {
   async sendCommand(@Body() { commands }: SendCommandDto) {
     commands = commands || []
 
-    for (const { id, actions: instructions } of commands) {
+    for (const { id, actions } of commands) {
+      const message: ICommandMessage = {
+        type: 'COMMAND',
+        payload: actions,
+      }
+
       await this.commandBus.execute(
         new SendMessageToTurtleCommand({
           turtleId: id,
-          message: JSON.stringify(instructions),
+          message: JSON.stringify(message),
         }),
       )
     }
